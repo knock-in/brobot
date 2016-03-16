992c8b0a85b567d5f4dc8bfef76a4f2b607a8240
Dardan <dard.ne@gmail.com>
2016-03-16 14:30:46 +0000

    morsemodule small fix when module is not called properly

--- src/modules/morse.js
+++ src/modules/morse.js
@@ -26,7 +26,13 @@ class MorseModule extends BaseModule {
              for(var i = 1;i < _args.length;i++) {
                 this._args.push(morse.decode(_args[i]));
             }
-        } 
+        } else {
+            // Neither encrypt or decrypt so return all arguments
+            this._args.push(this.token);
+            for(var i = 0;i < _args.length;i++) {
+                this._args.push(_args[i]);
+            }
+        }
     }
 }
 
