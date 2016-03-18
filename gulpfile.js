c96d83dd2fc73d2ffe172da638c47e91962ddacf
knock-in <dard.ne@gmail.com>
2016-03-18 22:33:56 +0100

    autodeploy test

--- gulpfile.js
+++ gulpfile.js
@@ -52,12 +52,8 @@ gulp.task('test', ['buildSrc'], function() {
 
 gulp.task('lint', function() {
     return gulp.src(SRC)
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console. 
-        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint()) 
         .pipe(eslint.format())
-        // To have the process exit with an error code (1) on 
-        // lint error, return the stream and pipe to failAfterError last. 
         .pipe(eslint.failAfterError());
 });
 
